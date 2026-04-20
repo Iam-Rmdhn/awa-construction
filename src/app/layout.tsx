d9f92bee002feddc157dc0280fdb78841782a380
iam-rmdhn <ilhamztr016@gmail.com>
2026-04-20 14:19:01 +0700

    feat: initialize root layout with fonts, providers, and global components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,33 @@ const nunitoSans = Nunito_Sans({
 
 export const metadata: Metadata = {
   title: 'Awa Construction',
-  description: '',
+  description: 'Jasa renovasi dan konsultasi gratis untuk kontruksi outlet ataupun ruko anda',
+  keywords: ['Awa Construction', 'Renovasi', 'Konsultasi', 'Kontruksi', 'Outlet', 'Ruko'],
+  authors: [{ name: 'Awa Construction' }],
+  creator: 'Awa Construction',
+  publisher: 'Awa Construction',
+  openGraph: {
+    title: 'Awa Construction',
+    description: 'Jasa renovasi dan konsultasi gratis untuk kontruksi outlet ataupun ruko anda',
+    url: 'https://awa-construction.vercel.app',
+    siteName: 'Awa Construction',
+    images: [
+      {
+        url: 'https://awa-construction.vercel.app/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Awa Construction',
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Awa Construction',
+    description: 'Jasa renovasi dan konsultasi gratis untuk kontruksi outlet ataupun ruko anda',
+    images: ['https://awa-construction.vercel.app/og-image.png'],
+  },
 };
 
 import { LanguageProvider } from '@/contexts/LanguageContext';
